b2d623ee7cc08ae3aa5a4cd223308f79a336a68a
Aman <diligentdwivedi@gmail.com>
2021-07-27 22:28:58 +0530

    second commit

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,32 +1,28 @@
-const expressJwt = require('express-jwt');
+const expressJwt = require("express-jwt");
 
 function authJwt() {
-    const secret = process.env.secret;
-    const api = process.env.API_URL;
-    return expressJwt({
-        secret,
-        algorithms: ['HS256'],
-        isRevoked: isRevoked
-    }).unless({
-        path: [
-            {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
-            `${api}/users/login`,
-            `${api}/users/register`,
-        ]
-    })
+  const secret = process.env.secret;
+  const api = process.env.API_URL;
+  return expressJwt({ secret, algorithms: ["HS256"], isRevoked }).unless({
+    path: [
+      // public routes that don't require authentication
+      /\/public\/uploads(.*)/,
+      `${api}/users/login`,
+      `${api}/users/register`,
+      { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/users(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/orders(.*)/, methods: ["GET", "OPTIONS", "POST"] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+    ],
+  });
 }
 
 async function isRevoked(req, payload, done) {
-    if(!payload.isAdmin) {
-        done(null, true)
-    }
+  if (!payload.isAdmin) {
+    done(null, true);
+  }
 
-    done();
+  done();
 }
 
-
-
-module.exports = authJwt
+module.exports = authJwt;
